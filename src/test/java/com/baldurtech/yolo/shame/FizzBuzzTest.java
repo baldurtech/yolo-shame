3139eae08503e0767f6dd8800032418d67740223
Chai Feng <chaifeng@chaifeng.com>
2014-05-19 10:30:40 +0800

    把剩下的几个测试用例也都替换成 assertEquals 方法来判断

--- src/test/java/com/baldurtech/yolo/shame/FizzBuzzTest.java
+++ src/test/java/com/baldurtech/yolo/shame/FizzBuzzTest.java
@@ -5,11 +5,10 @@ public class FizzBuzzTest {
     FizzBuzz app = new FizzBuzz();
 
     assertEquals("1", app.say(1));
-
-    System.out.println("Fizz".equals(app.say(3)));
-    System.out.println("Buzz".equals(app.say(5)));
-    System.out.println("Buzz".equals(app.say(10)));
-    System.out.println("FizzBuzz".equals(app.say(15)));
+    assertEquals("Fizz", app.say(3));
+    assertEquals("Buzz", app.say(5));
+    assertEquals("Buzz", app.say(10));
+    assertEquals("FizzBuzz", app.say(15));
   }
 
   public static void assertEquals(String expectedResult, String actualResult) {
