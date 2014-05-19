e5f1b672a6af000baafd0d9303e12340410fdbfe
Chai Feng <chaifeng@chaifeng.com>
2014-05-19 10:16:27 +0800

    直接输出测试的结果，原先还要我们自己去根据输出的参数来判断结果是否正确。

--- src/test/java/com/baldurtech/yolo/shame/FizzBuzzTest.java
+++ src/test/java/com/baldurtech/yolo/shame/FizzBuzzTest.java
@@ -4,10 +4,10 @@ public class FizzBuzzTest {
   public static void main(String[] args) {
     FizzBuzz app = new FizzBuzz();
 
-    System.out.println("1 == " + app.say(1));
-    System.out.println("3 == " + app.say(3));
-    System.out.println("5 == " + app.say(5));
-    System.out.println("10 == " + app.say(10));
-    System.out.println("15 == " + app.say(15));
+    System.out.println("1".equals(app.say(1)));
+    System.out.println("Fizz".equals(app.say(3)));
+    System.out.println("Buzz".equals(app.say(5)));
+    System.out.println("Buzz".equals(app.say(10)));
+    System.out.println("FizzBuzz".equals(app.say(15)));
   }
 }
