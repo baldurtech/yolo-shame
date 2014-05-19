ba111152d6be826028d800320fa59d9bae5c83c7
Chai Feng <chaifeng@chaifeng.com>
2014-05-19 09:59:40 +0800

    添加了 FizzBuzz 的测试用例

--- src/test/java/com/baldurtech/yolo/shame/FizzBuzzTest.java
+++ src/test/java/com/baldurtech/yolo/shame/FizzBuzzTest.java
@@ -1,5 +1,13 @@
 package com.baldurtech.yolo.shame;
 
 public class FizzBuzzTest {
-  
+  public static void main(String[] args) {
+    FizzBuzz app = new FizzBuzz();
+    
+    System.out.println(app.say(1));
+    System.out.println(app.say(3));
+    System.out.println(app.say(5));
+    System.out.println(app.say(10));
+    System.out.println(app.say(15));
+  }
 }
