99645012831ea863899c4da00c80daedadb8538e
Chai Feng <chaifeng@chaifeng.com>
2014-05-19 10:27:00 +0800

    把输出测试结果的代码抽取到方法 assertEquals 里

--- src/test/java/com/baldurtech/yolo/shame/FizzBuzzTest.java
+++ src/test/java/com/baldurtech/yolo/shame/FizzBuzzTest.java
@@ -6,11 +6,15 @@ public class FizzBuzzTest {
 
     String expectedResult = "1";
     String actualResult = app.say(1);
-    System.out.println(expectedResult.equals(actualResult));
+    assertEquals(expectedResult, actualResult);
 
     System.out.println("Fizz".equals(app.say(3)));
     System.out.println("Buzz".equals(app.say(5)));
     System.out.println("Buzz".equals(app.say(10)));
     System.out.println("FizzBuzz".equals(app.say(15)));
+  }
+
+  public static void assertEquals(String expectedResult, String actualResult) {
+    System.out.println(expectedResult.equals(actualResult));
   }
 }
