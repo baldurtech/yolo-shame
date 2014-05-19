843378712a601020ce38e2202da81899a30f35e6
Chai Feng <chaifeng@chaifeng.com>
2014-05-19 10:34:11 +0800

    重新缩进源码

--- src/test/java/com/baldurtech/yolo/shame/FizzBuzzTest.java
+++ src/test/java/com/baldurtech/yolo/shame/FizzBuzzTest.java
@@ -1,17 +1,17 @@
 package com.baldurtech.yolo.shame;
 
 public class FizzBuzzTest {
-  public static void main(String[] args) {
-    FizzBuzz app = new FizzBuzz();
+    public static void main(String[] args) {
+        FizzBuzz app = new FizzBuzz();
 
-    assertEquals("1", app.say(1));
-    assertEquals("Fizz", app.say(3));
-    assertEquals("Buzz", app.say(5));
-    assertEquals("Buzz", app.say(10));
-    assertEquals("FizzBuzz", app.say(15));
-  }
+        assertEquals("1", app.say(1));
+        assertEquals("Fizz", app.say(3));
+        assertEquals("Buzz", app.say(5));
+        assertEquals("Buzz", app.say(10));
+        assertEquals("FizzBuzz", app.say(15));
+    }
 
-  public static void assertEquals(String expectedResult, String actualResult) {
-    System.out.println(expectedResult.equals(actualResult));
-  }
+    public static void assertEquals(String expectedResult, String actualResult) {
+        System.out.println(expectedResult.equals(actualResult));
+    }
 }
