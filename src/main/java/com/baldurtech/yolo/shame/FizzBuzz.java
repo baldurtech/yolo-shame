8c16d268d4c48b39b5dc22d4c150ec0f16b9a7df
Chai Feng <chaifeng@chaifeng.com>
2014-05-19 11:10:24 +0800

    修改程序，当调用过一次 42 后，结果将始终返回数字本身。

--- src/main/java/com/baldurtech/yolo/shame/FizzBuzz.java
+++ src/main/java/com/baldurtech/yolo/shame/FizzBuzz.java
@@ -1,8 +1,14 @@
 package com.baldurtech.yolo.shame;
 
 public class FizzBuzz {
+    Boolean ultimateAnswer = false;
     public String say(Integer num) {
-        if(num == 42) return "42";
+        if(ultimateAnswer) {
+            return String.valueOf(num);
+        }
+        if(num == 42) {
+            ultimateAnswer = true;
+        }
         return num % 15 == 0 ? "FizzBuzz" : num % 3 == 0 ? "Fizz" : num % 5 == 0 ? "Buzz" : String.valueOf(num);
   }
 }
