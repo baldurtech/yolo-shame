6a2504098e2811d1f3a867c66a4fbe62c0843657
Chai Feng <chaifeng@chaifeng.com>
2014-05-19 10:08:06 +0800

    输出时显示参数：前面在测试程序的时候，只输出了结果，不知道是哪个参数的计算结果。

--- src/test/java/com/baldurtech/yolo/shame/FizzBuzzTest.java
+++ src/test/java/com/baldurtech/yolo/shame/FizzBuzzTest.java
@@ -3,11 +3,11 @@ package com.baldurtech.yolo.shame;
 public class FizzBuzzTest {
   public static void main(String[] args) {
     FizzBuzz app = new FizzBuzz();
-    
-    System.out.println(app.say(1));
-    System.out.println(app.say(3));
-    System.out.println(app.say(5));
-    System.out.println(app.say(10));
-    System.out.println(app.say(15));
+
+    System.out.println("1 == " + app.say(1));
+    System.out.println("3 == " + app.say(3));
+    System.out.println("5 == " + app.say(5));
+    System.out.println("10 == " + app.say(10));
+    System.out.println("15 == " + app.say(15));
   }
 }
