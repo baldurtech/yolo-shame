cda38a082324e8499d70600371190db5b8082241
Chai Feng <chaifeng@chaifeng.com>
2014-05-19 10:23:42 +0800

    发现了大量的重复判断逻辑，开始重构，把第一个测试用例的预期结果和实际结果 抽取出来

--- src/test/java/com/baldurtech/yolo/shame/FizzBuzzTest.java
+++ src/test/java/com/baldurtech/yolo/shame/FizzBuzzTest.java
@@ -4,7 +4,10 @@ public class FizzBuzzTest {
   public static void main(String[] args) {
     FizzBuzz app = new FizzBuzz();
 
-    System.out.println("1".equals(app.say(1)));
+    String expectedResult = "1";
+    String actualResult = app.say(1);
+    System.out.println(expectedResult.equals(actualResult));
+
     System.out.println("Fizz".equals(app.say(3)));
     System.out.println("Buzz".equals(app.say(5)));
     System.out.println("Buzz".equals(app.say(10)));
