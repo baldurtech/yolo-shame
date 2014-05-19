799b19f4b33830c697dda2828315b39072841649
Chai Feng <chaifeng@chaifeng.com>
2014-05-19 10:28:26 +0800

    把第一个测试用例需要的两个变量 inline

--- src/test/java/com/baldurtech/yolo/shame/FizzBuzzTest.java
+++ src/test/java/com/baldurtech/yolo/shame/FizzBuzzTest.java
@@ -4,9 +4,7 @@ public class FizzBuzzTest {
   public static void main(String[] args) {
     FizzBuzz app = new FizzBuzz();
 
-    String expectedResult = "1";
-    String actualResult = app.say(1);
-    assertEquals(expectedResult, actualResult);
+    assertEquals("1", app.say(1));
 
     System.out.println("Fizz".equals(app.say(3)));
     System.out.println("Buzz".equals(app.say(5)));
