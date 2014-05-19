99f108da1c12fd42ca21742c8fca9928efb34187
Chai Feng <chaifeng@chaifeng.com>
2014-05-19 09:59:24 +0800

    实现了 FizzBuzz

--- src/main/java/com/baldurtech/yolo/shame/FizzBuzz.java
+++ src/main/java/com/baldurtech/yolo/shame/FizzBuzz.java
@@ -1,5 +1,7 @@
 package com.baldurtech.yolo.shame;
 
 public class FizzBuzz {
-  
+  public String say(Integer num) {
+    return num % 15 == 0 ? "FizzBuzz" : num % 3 == 0 ? "Fizz" : num % 5 == 0 ? "Buzz" : String.valueOf(num);
+  }
 }
