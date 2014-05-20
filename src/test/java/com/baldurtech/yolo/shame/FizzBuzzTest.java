7c18fd154a146eec00482afe9292f2305e55279e
Chai Feng <chaifeng@chaifeng.com>
2014-05-20 10:36:29 +0800

    把 FizzBuzzTest 里面与测试案例无关的代码抽取到父类 YoloTestCase 中。

--- src/test/java/com/baldurtech/yolo/shame/FizzBuzzTest.java
+++ src/test/java/com/baldurtech/yolo/shame/FizzBuzzTest.java
@@ -1,37 +1,6 @@
 package com.baldurtech.yolo.shame;
 
-import java.lang.reflect.Method;
-
 public class FizzBuzzTest extends YoloTestCase {
-    static Boolean testResult = true;
-    static Integer countSuccess = 0;
-    static Integer countFail = 0;
-
-    public static void main(String[] args) {
-        runTest();
-    }
-
-    public static void runTest() {
-        Method[] methods = FizzBuzzTest.class.getDeclaredMethods();
-        for(Method method: methods) {
-            System.out.println("method: " + method.getName());
-            if(method.getName().startsWith("test")) {
-                System.out.println("  invoke: " + method.getName());
-                try {
-                    /*
-                       下面的代码等同于 new FizzBuzzTest().method()
-                       但是这个 method 是一个以 test 开头的方法，而且没有参数。
-                     */
-                    method.invoke(new FizzBuzzTest());
-                } catch(Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-
-        outputTestReport();
-    }
-
     FizzBuzz app = new FizzBuzz();
 
     public void test_1() {
@@ -62,20 +31,5 @@ public class FizzBuzzTest extends YoloTestCase {
 
     public void test_15() {
         assertEquals("FizzBuzz", app.say(15));
-    }
-
-    public static void assertEquals(String expectedResult, String actualResult) {
-        if(expectedResult.equals(actualResult)) {
-            countSuccess ++;
-        } else {
-            countFail ++;
-            System.out.println("Expected " + expectedResult + ", but result is " + actualResult);
-            testResult = false;
-        }
-    }
-
-    public static void outputTestReport() {
-        System.out.println("Test result: " + (testResult ? "SUCCESS" : "FAIL"));
-        System.out.println("Test pass: " + countSuccess + ", test fail: " + countFail);
     }
 }
