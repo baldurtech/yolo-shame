ea09e9f330a29ec517b8993b9e3e9319f97dc889
Chai Feng <chaifeng@chaifeng.com>
2014-05-25 10:10:42 +0800

    把运行测试的代码都移动到 YoloTestRunner

--- src/test/java/com/baldurtech/yolo/shame/YoloTestCase.java
+++ src/test/java/com/baldurtech/yolo/shame/YoloTestCase.java
@@ -1,42 +1,12 @@
 package com.baldurtech.yolo.shame;
 
-import java.lang.reflect.Method;
-
 public abstract class YoloTestCase {
     static Boolean testResult = true;
     static Integer countSuccess = 0;
     static Integer countFail = 0;
 
     public static void main(String[] args) {
-        runTest(FizzBuzzTest.class);
-    }
-
-    public static void runTest(Class testCaseClazz) {
-        if(testCaseClazz.getSuperclass() != YoloTestCase.class) {
-            return;
-        }
-        Method[] methods = testCaseClazz.getDeclaredMethods();
-        for(Method method: methods) {
-            System.out.println("method: " + method.getName());
-            if(method.getName().startsWith("test")) {
-                System.out.println("  invoke: " + method.getName());
-                try {
-                    /*
-                       下面的代码等同于 new FizzBuzzTest().method()
-                       但是这个 method 是一个以 test 开头的方法，而且没有参数。
-                     */
-                    method.invoke(testCaseClazz.newInstance());
-                } catch(Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-
-        outputTestReport();
-    }
-
-    public static void runTest(YoloTestCase yoloTestCase) {
-        runTest(yoloTestCase.getClass());
+        YoloTestRunner.runTest(FizzBuzzTest.class);
     }
 
     public static void assertEquals(String expectedResult, String actualResult) {
@@ -48,10 +18,4 @@ public abstract class YoloTestCase {
             testResult = false;
         }
     }
-
-    public static void outputTestReport() {
-        System.out.println("Test result: " + (YoloTestCase.testResult ? "SUCCESS" : "FAIL"));
-        System.out.println("Test pass: " + YoloTestCase.countSuccess + ", test fail: " + YoloTestCase.countFail);
-    }
-
 }
--- src/test/java/com/baldurtech/yolo/shame/YoloTestRunner.java
+++ src/test/java/com/baldurtech/yolo/shame/YoloTestRunner.java
@@ -1,14 +1,50 @@
 package com.baldurtech.yolo.shame;
 
+import java.lang.reflect.Method;
+
 public class YoloTestRunner {
     public static void main(String[] args) {
         try {
             for(String clazzName: args) {
                 YoloTestCase testCase = (YoloTestCase) Class.forName(clazzName).newInstance();
-                testCase.runTest(testCase.getClass());
+                runTest(testCase.getClass());
             }
         } catch(Exception e) {
 
         }
     }
+
+    public static void runTest(Class testCaseClazz) {
+        if(testCaseClazz.getSuperclass() != YoloTestCase.class) {
+            return;
+        }
+        Method[] methods = testCaseClazz.getDeclaredMethods();
+        for(Method method: methods) {
+            System.out.println("method: " + method.getName());
+            if(method.getName().startsWith("test")) {
+                System.out.println("  invoke: " + method.getName());
+                try {
+                    /*
+                       下面的代码等同于 new FizzBuzzTest().method()
+                       但是这个 method 是一个以 test 开头的方法，而且没有参数。
+                     */
+                    method.invoke(testCaseClazz.newInstance());
+                } catch(Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        outputTestReport();
+    }
+
+    public static void runTest(YoloTestCase yoloTestCase) {
+        runTest(yoloTestCase.getClass());
+    }
+
+    public static void outputTestReport() {
+        System.out.println("Test result: " + (YoloTestCase.testResult ? "SUCCESS" : "FAIL"));
+        System.out.println("Test pass: " + YoloTestCase.countSuccess + ", test fail: " + YoloTestCase.countFail);
+    }
+
 }
